be35fd2996e1c8ee4a3af322590ee584c5cf55aa
4th3rine <catherinefrancoeur@hotmail.com>
2016-04-04 11:32:07 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE-edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link href="style.css" rel="stylesheet">
+		<link href="nav.css" rel="stylesheet">
+		<link href="header.css" rel="stylesheet">
+		<link href="milieu.css" rel="stylesheet">
+		<title>Forum Jeunesse de Charlevoix-Ouest</title>
+	</head>
+
+	<body>
+
+
+
+		<!--Menu Fixe-->
+		  <nav>
+		 <!--ajouter le logo ici-->
+		    <ul>
+		      <li>
+		        <a href="#">Milieu de vie</a>
+		      </li>
+		      <li>
+		        <a href="#">Services</a>
+		      </li>
+		      <li>
+		        <a href="#">A propos</a>
+		      </li>
+		      <li>
+		        <a href="#">Équipe</a>
+		      </li>
+		      <li>
+		        <a href="#">Galerie</a>
+		      </li>
+		      <li>
+		        <a href="#">Ressources</a>
+		      </li>
+		      <li>
+		        <a href="#">Documentation</a>
+		      </li>
+		      <li>
+		        <a href="#">Partenaires financiers</a>
+		      </li>
+		      <li>
+		        <a href="#">Nous joindre</a>
+		      </li>
+		    </ul>
+  		</nav>
+
+		<!--Petit Menu Fixe-->
+		  <nav id="small">
+		 <img src="images/LOGO_2012_FORUM_JEUNESSE_2.png">
+		    <ul>
+				<li>
+		        <a href="#">Milieu de vie</a>
+		      </li>
+		      <li>
+		        <a href="#">Services</a>
+		      </li>
+		      <li>
+		        <a href="#">A propos</a>
+		      </li>
+		      <li>
+		        <a href="#">Équipe</a>
+		      </li>
+		      <li>
+		        <a href="#">Galerie</a>
+		      </li>
+		      <li>
+		        <a href="#">Ressources</a>
+		      </li>
+		      <li>
+		        <a href="#">Documentation</a>
+		      </li>
+		      <li>
+		        <a href="#">Partenaires financiers</a>
+		      </li>
+		      <li>
+		        <a href="#">Nous joindre</a>
+		      </li>
+		    </ul>
+		  </nav>
+
+		  <!--Header avec slider-->
+			
+	<header>
+		<div id="captioned-gallery">
+			<figure class="slider">
+				<figure>
+					<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/hobbiton-lake.jpg" alt>
+				</figure>
+				<figure>
+					<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/wanaka-drowned-tree.jpg" alt>
+				</figure>
+				<figure>
+					<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/utah-peak.jpg" alt>
+				</figure>
+				<figure>
+					<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/bryce-canyon-utah.jpg" alt>
+				</figure>
+				<figure>
+					<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/hobbiton-lake.jpg" alt>
+				</figure>
+			</figure>
+		</div>
+	</header>
+
+	<!--milieu de vie-->
+			<section class="no-touch">
+
+				  <div class="wrap">
+				    
+				    <div class="box">
+				      <div class="boxInner">
+				        <img src="http://www.dwuser.com/education/content/creating-responsive-tiled-layout-with-pure-css/images/demo/1.jpg" />
+				        <div class="titleBox">An old greenhouse</div>
+				      </div>
+				    </div>
+				    <div class="box">
+				      <div class="boxInner">
+				        <img src="http://www.dwuser.com/education/content/creating-responsive-tiled-layout-with-pure-css/images/demo/1.jpg" />
+				        <div class="titleBox">An old greenhouse</div>
+				      </div>
+				    </div>
+				    
+				    <div class="box">
+				      <div class="boxInner">
+				        <img src="http://www.dwuser.com/education/content/creating-responsive-tiled-layout-with-pure-css/images/demo/2.jpg"  />
+				        <div class="titleBox">Purple wildflowers</div>
+				      </div>
+				    </div>
+				    <div class="box">
+				      <div class="boxInner">
+				        <img src="http://www.dwuser.com/education/content/creating-responsive-tiled-layout-with-pure-css/images/demo/3.jpg" />
+				        <div class="titleBox">A birdfeeder</div>
+				      </div>
+				    </div>
+				    <div class="box">
+				      <div class="boxInner">
+				        <img src="http://www.dwuser.com/education/content/creating-responsive-tiled-layout-with-pure-css/images/demo/10.jpg" />
+				        <div class="titleBox">Crocus close-up</div>
+				      </div>
+				    </div>
+				  </div>
+	
+			</section>	
+
+		<!--Services-->
+		<div id="services">
+			
+		</div>
+		
+	</body>
+</html>
